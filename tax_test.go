d4e876021d21ac79a927d5e4bd240efa5a4f560f
Gary Willoughby <dev@nomad.uk.net>
2022-10-09 16:59:03 +0100

    Removed unnecessary test.

--- tax_test.go
+++ tax_test.go
@@ -5,23 +5,6 @@ import (
 	"testing"
 )
 
-func TestAddTax(t *testing.T) {
-	t1, _ := MoneyGBP(0)
-	t2, _ := MoneyGBP(1387)
-	t3, _ := MoneyGBP(457)
-
-	taxes := taxes{
-		total:  t1,
-		detail: make(map[string]Money, 0),
-	}
-
-	taxes = taxes.add("VAT", t2)
-	taxes = taxes.add("VAT", t3)
-
-	assertMoneyValue(t, taxes.total, 1844)
-	assertMoneyValue(t, taxes.detail["VAT"], 1844)
-}
-
 func TestTaxJsonMarshalling(t *testing.T) {
 	t1, _ := MoneyGBP(0)
 	t2, _ := MoneyGBP(1387)
